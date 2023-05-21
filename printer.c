c4305401fcc2a7fbc5b44c1212bda0023dd6d36e
Med Dev <medterch@gmail.com>
2023-05-21 16:12:55 -0700

    shell_pro com1

---
+++ printer.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+/**
+ * print_number - Function print Unsigned int
+ * @n: Unsigned Integer
+ * Return: Nothing
+ */
+void print_number(unsigned int n)
+{
+	unsigned int x = n;
+
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
+/**
+ * print_number_in - Function print Number
+ * @n:Integer
+ * Return: void
+ */
+void print_number_in(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
